c93a9b73c0b4e0028e9846e46680b04e21eb10b7
davidnevadoc <nevadod@gmx.com>
2016-03-16 19:05:38 +0100

    autocommit

--- autopush.sh
+++ autopush.sh
@@ -1,4 +1,4 @@
 git add --all
 git commit -m 'autocommit'
-git remote add origin2 https://github.com/pablomm/QwertySoper
-git push origin2 master
+git remote add origin https://github.com/pablomm/QwertySoper
+git push origin master
